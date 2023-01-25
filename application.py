abab5cd4d96e31247dc6ff50ede40968a43216b8
ASabovic <74186547+ASabovic@users.noreply.github.com>
2023-01-25 15:22:27 +0100

    new commit 2

--- application.py
+++ application.py
@@ -4,11 +4,6 @@ def sum_function():
 def subtraction_function():
     print("This is the subtraction function")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 4224ff28f236d3f632872af319e4579794667b66
 def multiplication_function():
     print("This is the multiplication function")
 
@@ -20,20 +15,16 @@ def simple_function():
 
 def complex_function():
     print("This is the complex function")
-<<<<<<< HEAD
-=======
 
 def supercomplex_function():
     print("This is the super complex function")
->>>>>>> 4224ff28f236d3f632872af319e4579794667b66
+
 
 sum_function()
 subtraction_function()
 multiplication_function()
 division_function()
 simple_function()
-<<<<<<< HEAD
-=======
 complex_function()
 supercomplex_function()
->>>>>>> 4224ff28f236d3f632872af319e4579794667b66
+
