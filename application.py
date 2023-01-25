d59244ae49bbf9868f30543d804393c011fc69dd
ASabovic <74186547+ASabovic@users.noreply.github.com>
2023-01-25 15:00:39 +0100

    Cherry-pick performed

--- application.py
+++ application.py
@@ -1,8 +1,26 @@
 def sum_function():
     print("This is the sum function!")
 
-def deduction_function():
-    print("This is the deduction function")
+def subtraction_function():
+    print("This is the subtraction function")
 
+<<<<<<< HEAD
+=======
+def multiplication_function():
+    print("This is the multiplication function")
+
+def division_function():
+    print("This is the division function")
+
+def simple_function():
+    print("This is the simple function")
+
+def complex_function():
+    print("This is the complex function")
+
+>>>>>>> bf094fc (Application python file is modified. Two new functions are added, some function names are changed.)
 sum_function()
-deduction_function()
+subtraction_function()
+multiplication_function()
+division_function()
+simple_function()
